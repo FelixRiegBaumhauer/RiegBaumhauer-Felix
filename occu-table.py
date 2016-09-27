b700a0a8c1e088881563ba025ad376365e12b500
FelixRiegBaumhauer <friegbaumhauer@stuy.edu>
2016-09-27 02:12:33 -0400

    Update occu-table.py

--- occu-table.py
+++ occu-table.py
@@ -1,82 +1,111 @@
 from flask import Flask, render_template
-import random
 
 app=Flask(__name__)
+
+import random
+
 
 @app.route("/") #recyclyed, 
 def helloworld():
     return "To access, type occupations in the top thing"
 
+#breka things up and open things, we break along /n
 
-@app.route("/StepOne")
-def step1():
-    return "you completted step 1"
-
-@app.route("/StepTwo")
-def step2():
-    return "you completed step 2"
-
-@app.route("/StepThree")
-def step3():
-    return "step three completed, \n 100% completion, \n... \n... \n    Noah"
-
-#@app.route("/FirstTemp")
-#def test_temp():
-#    return render_template("temp.html", foo ="This is the best title", fool=di#ct)should return a render template
-
-#---------------------------------------------------------#
-#The other code
+def builder(inFile):
+    #file=open("occupations.csv", "r")
+    file=open(inFile, "r")
+    inputString = file.read()
+    brokenInput = str.split(inputString, "\r\n")
 
 
-# Opens and reads file into stringthing
-file=open("occupations.csv", "r")
-stringthing = file.read()
-
-# Splits the string into an array called splitString
-splitString = str.split(stringthing, "\r\n")
-
-dict = {}
-
-# Loops through array line by line
-for line in splitString:
-	if "Total" not in line:
-		if len(line)>0 and line[0]=='"':
-			line = line[1:]
-			dict[float(line[line.index('"')+2:])]=line[0:line.index('"')]
-		elif len(line)>0 and splitString.index(line)!=0:
-			#print line
-			dict[float(line[line.index(',')+1:])]=line[0:line.index(',')]
-        
+    #dict made, VITAL
+    dict = {}
+    
+    
+    #Got rid of non needed info
+    refinedInput = brokenInput[1:-2]
+    
+    
+    #To put every thing in a dict, with the following order
+    #keys = job
+    #values = percentage of labor market
+    
+    #This blurb of code puts out read in values in a dict
+    for line in refinedInput:
+        job = ""
+        num = ""
+        if(line[0] == '"'):
+            line = line[1:]
+            run = len(line)-1
+            while (run>=0):
+                if(line[run] == '"'):
+                    job = line[:run]
+                    num = line[run+2:]
+                    run = 0
+                    dict[job]=num
+                    #dict[num] = job
+                    #print job+"----"+num
+                run-=1
+        else:
+            pos = line.index(",")
+            job = line[:pos]
+            num = line[pos+1:]
+            dict[job]=num
+            #dict[num] = job
+            #print(job+"---"+num)
+    return dict
+            
 #print dict
 
-#because we have to make this a function...
-def getRandomOccupation():
-	#get a random number from 0 to 997 <-- although the toal is 99.8, we're working with integers and randint() is inclusive of zero
-	r = random.randint(0,997)
-	temp = 0 #keeps track of probability range
+dict = builder("occupations.csv")
+
+#Gets a random job, by means of generating a random int, then using the values stored in the
+#dict to find an appropriate value
+def getRandomOccupation(dict):
+    randInt = random.randint(0, 997)
+    posHold = 0
+    for key in dict:
+        posHold += (10 * float(dict[key]))
+        if(randInt < posHold):
+            return key
+    #If all goes wrong
+    return "No job, Error"+(str(posHold))+":"+(str(randInt))
+
+
+#quick Test, tests quicklly
+def quickTest():
+	results = {}
+	#lengthHold = len(dict)
+	#while (lengthHold > 0):
 	for key in dict:
-		temp += 10 * key
-		if r < temp:
-			return dict[key]
-		if temp == 859:
-			return dict[key]
+		results[key]=0
+	counter=0
+	while (counter < 10000):
+		counter+=1
+		rand = getRandomOccupation()
+		results[rand] = results[rand]+1
+	output = ""
+	for key in results:
+            val = (str)(results[key] / 100.00)
+	    #output+=(key+" happend "+(str)(results[key])+"/10000 times\n")#key in results is the name of proffesion
+            output+=key+" had a "+val+"% rating\n"
+        return output
+
+#Use this to run
+#print "\nRandomlly chosen job: "+getRandomOccupation(dict)+"\n"
+
+#Use this to validate the correctness of the code
+#print quickTest()
     
-        
-#print getRandomOccupation()
-#print quickText()
 
-#jobs = []
-#nums = []
-#for key in dict:
-#    nums.append(key)
-#    jobs.append(dict[key])
 
-file.close()
+#--------------------------------------#
 
 
 @app.route("/occupations")
 def occupater():
-    return render_template("occu-temp.html",coll=dict, name="A Collection of Occupations", ans = getRandomOccupation())#should return a render template
+    return render_template("occu-temp.html", title="A Collection of Occupations", dict = dict, ans = getRandomOccupation(dict))
+#should return a render template
 
     
 if __name__=="__main__":
